3e7f9a1a64d6bf4ee3186e1a94a929df37fc7b05
punjitha <132387971+algotyrnt@users.noreply.github.com>
2026-02-25 17:04:03 +0530

    refactor BeaconApp to introduce DependencyRootView for improved state management and environment object usage

--- Beacon.swiftpm/BeaconApp.swift
+++ Beacon.swiftpm/BeaconApp.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 @main
 struct BeaconApp: App {
+    @StateObject private var engine = BeaconEngine()
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            DependencyRootView(engine: engine, locationManager: locationManager)
         }
+    }
+}
+
+struct DependencyRootView: View {
+    @ObservedObject var engine: BeaconEngine
+    @ObservedObject var locationManager: LocationManager
+    
+    @StateObject private var vm: BeaconViewModel
+    
+    init(engine: BeaconEngine, locationManager: LocationManager) {
+        self.engine = engine
+        self.locationManager = locationManager
+        _vm = StateObject(wrappedValue: BeaconViewModel(engine: engine, locationManager: locationManager))
+    }
+    
+    var body: some View {
+        MainTabView()
+            .environmentObject(engine)
+            .environmentObject(locationManager)
+            .environmentObject(vm)
     }
 }
